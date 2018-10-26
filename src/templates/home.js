a34b6704621a5b320c50a1442ba9f88717846c76
Nicolas Fetter <nicofetter@gmail.com>
2018-10-26 15:12:11 +1300

    Fixing 404 page, and importing graphql instead of using global var

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
